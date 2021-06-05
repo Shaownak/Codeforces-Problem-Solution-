626a3b08c6c395e78d8be52c602e5c3dd321af7b
Shaownak Shahriar <63370410+Shaownak@users.noreply.github.com>
2021-06-06 04:18:04 +0600

    Create A_Calculating_Function.cpp

---
+++ A_Calculating_Function.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define lli long long int
+#define FOR(i,a,b) for(int i=a; i<b; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define si set<int>
+#define ss set<string>
+#define mii map<int,int>
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    //TODO: Start your code here:
+
+    lli n; cin>>n;
+
+    if(n%2==0){
+        cout<<n/2<<endl;
+    }
+    else cout<<(-(n+1))/2<<endl;
+
+
+
+    return 0;
+}
