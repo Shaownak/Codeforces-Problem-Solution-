a6151f8b189a609fff56166c097f6a79cbf4b398
Shaownak Shahriar <63370410+Shaownak@users.noreply.github.com>
2021-06-07 03:09:56 +0600

    Create B_Fedor_the_New_Game.cpp

---
+++ B_Fedor_the_New_Game.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define lli long long int
+#define FOR(i,a,b) for(int i=a; i<b; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define si set<int>
+#define ss set<string>
+#define mii map<int,int>
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    //TODO: Start your code here:
+
+    vi a(10000);
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    for (int i = 0; i < m + 1; i++)
+        cin >> a[i];
+
+    int friends = 0;
+
+    for (int i = 0; i < m; i++)
+    {
+        int t = 0;
+        for (int j = 0; j < n; j++)
+            if (((a[i] >> j) & 1) != ((a[m] >> j) & 1))
+                t++;
+        if(t <= k)
+            friends++;
+    }
+
+    cout << friends << endl;
+    return 0;
+
+
+
+    return 0;
+}
