3421ccabf812b50ec3312e7dee60aeda471b9e43
Shaownak Shahriar <63370410+Shaownak@users.noreply.github.com>
2021-06-08 02:26:01 +0600

    Add files via upload

---
+++ C_Product_1_Modulo_N.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define lli long long int
+#define FOR(i,a,b) for(int i=a; i<b; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define si set<int>
+#define ss set<string>
+#define mii map<int,int>
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    //TODO: Start your code here:
+
+    cout<<4%5;
+
+
+
+    return 0;
+}
