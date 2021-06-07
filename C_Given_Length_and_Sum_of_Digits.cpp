2d0f204554db8f11b932ac8afa79291c4c65c210
Shaownak Shahriar <63370410+Shaownak@users.noreply.github.com>
2021-06-07 14:22:03 +0600

    Create C_Given_Length_and_Sum_of_Digits.cpp

---
+++ C_Given_Length_and_Sum_of_Digits.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define lli long long int
+#define FOR(i,a,b) for(int i=a; i<b; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define si set<int>
+#define ss set<string>
+#define mii map<int,int>
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    //TODO: Start your code here:
+
+    int m, s, t, i;
+    cin >> m >> s;
+    string a, b;
+
+    if (s == 0)
+    {
+        cout << (m == 1 ? "0 0" : "-1 -1") << endl;
+        return 0;
+    }
+
+    for (i = 0; i < m; i++)
+    {
+        t = min(s, 9);
+        b += t + '0';
+        s -= t;
+    }
+
+    if (s > 0)
+    {
+        cout << "-1 -1" << endl;
+        return 0;
+    }
+
+    for (i = m - 1; i >= 0; i--)
+        a += b[i];
+
+    for (i = 0; a[i] == '0'; i++);
+
+    a[i]--, a[0]++;
+    cout << a << " " << b << endl;
+
+
+
+    return 0;
+}
